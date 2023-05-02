c96a59863ddfae7d38be085a9111417619e085ea
SandeepJoshi <joshisandip111@gmail.com>
2023-05-02 18:12:26 +0545

    Initialize

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './header.css'
+import LOGO from '../../assets/high jott.png'
+
 const Header = () => {
   return (
     <div className='header'>
-            <h1>JOTT IN</h1>
+            <img src={LOGO} alt="" />
             <h2>JOT THEM DOWN</h2>
     </div>
   )
